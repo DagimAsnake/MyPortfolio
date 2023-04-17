75b086149b684fcf2099c2674ba945176ab147d5
Dagim Asnake <dagimasnake1@gmail.com>
2023-04-17 09:54:45 -1200

    added social links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import SocialLinks from "./components/SocialLinks";
 
 function App() {
   return (
    <>
    <Navbar />
    <Home />
+   <SocialLinks />
    </>
   );
 }
