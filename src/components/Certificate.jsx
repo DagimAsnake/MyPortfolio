5e6fde949b21cfb5fac189cddc0e6e5576293cb0
Dagim Asnake <dagimasnake1@gmail.com>
2023-10-25 17:27:44 -1200

    fix bug

--- src/components/Certificate.jsx
+++ src/components/Certificate.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import php from '../assets/certificate/Php.jpeg'
 import python from '../assets/certificate/python.png'
 import UdemyPython from '../assets/certificate/UdemyPython.jpg'
-import UdemyPyBeg from '../assets/certificate/UdemyPyBeg.jpg'
+// import UdemyPyBeg from '../assets/certificate/UdemyPyBeg.jpg'
 import Docker from "../assets/certificate/Docker.jpg" 
 import UdemyReact from "../assets/certificate/UdemyReact.jpeg"
 import UdemyNextjs from "../assets/certificate/UdemyNextjs.jpg"
